c802590f6d58579c4d488a4e60c3e0767f928a3e
Igor <k007.igor@gmail.com>
2026-09-09 12:29:17 +0500

    Первый коммит, настройка проекта

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <main className='frame'>
+        <section className='sitebar'></section>
+      </main>
+    </>
+  );
+}
+
+export default App;
